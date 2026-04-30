152c84effc9859b2b8aa69886325c5f4e4d34ba2
connorelzingastu-sudo <connor.elzinga.stu@cedarpark.org>
2026-04-30 12:40:37 -0700

    Update day3.py

--- day3.py
+++ day3.py
@@ -10,17 +10,31 @@ def display_pokemon(data):
     print("Height:", data["height"])
     print("Weight:", data["weight"])
     print("Base Experience:", data["base_experience"])
-    print("\nTypes:")
-
+    
+    # Types
+    type_names = []
     for type_info in data["types"]:
-        print("-", type_info["type"]["name"])
+        type_names.append(type_info["type"]["name"])
+    print("\nTypes: "  + ", ".join(type_names))
     
     print("\nAbilities:")
-
     for ability_info in data["abilities"]:
        print("-", ability_info["ability"]["name"])
     
-    print(data["stats"])
+    print("\nBase Stats:")
+    highest_stat_value = 0
+    highest_stat_name = ""
+    for stat_info in data["stats"]:
+        stat_name = stat_info["stat"]["name"]
+        clean_stat_name = stat_name.replace("-"," ").title()
+        stat_value = int(stat_info["base_stat"])
+        print(f"- {clean_stat_name}: {stat_value}")
+
+        # Check for higher stats
+        if stat_value > highest_stat_value:
+            highest_stat_value = stat_value
+            highest_stat_name = clean_stat_name
+    print(f"Strongest Stat: {highest_stat_name} with {highest_stat_value}")
 
 def main():
     while True:
