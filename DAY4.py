94c51d48c8942300b155b74454dbfb67054437f7
connorelzingastu-sudo <connor.elzinga.stu@cedarpark.org>
2026-04-30 20:29:12 -0700

    Update DAY4.py
    clean up

--- DAY4.py
+++ DAY4.py
@@ -1,9 +1,12 @@
 import requests
 
-print("Welcome to the Python Pokédex!")
-print("Search for Pokémon and view their information.")
-print("")
+# Displays a message to welcome users
+def welcome_message():
+    print("Welcome to the Python Pokédex!")
+    print("Search for Pokémon and view their information.")
+    print()
 
+# Takes pokemon 'data' and displays it in a pretty format
 def display_pokemon(data):
     print()
     print("======================")
@@ -42,26 +45,34 @@ def display_pokemon(data):
 
 
 
-#pass a pokemon name to get the api response if no response is found it will display a message
+# Pass a pokemon name to get the api response. If no response is found it will display a message
 def get_pokemon_data(pokemon_name):
     url = f"https://pokeapi.co/api/v2/pokemon/{pokemon_name}"
     response = requests.get(url)
 
     if response.status_code == 200:
         data = response.json()
-        display_pokemon(data)
+        return data
     else:
         print("Pokémon not found.")
 
 def main():
+    welcome_message()
+
+    # Main program loop
     while True:
+        # User input
         pokemon_name = input("Enter a Pokémon name / Id, (or 'quit' to exit): ").lower()
 
+        # Check for exit condition
         if pokemon_name == "quit":
             print("Goodbye")
             break
 
-        get_pokemon_data(pokemon_name)
+        # Get and display pokemon data for the input if available
+        data = get_pokemon_data(pokemon_name)
+        if data:
+            display_pokemon(data)
 main()
 
 
